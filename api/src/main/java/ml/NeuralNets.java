4f44b90cd9dc5b587400056567a7628a52c4df13
Matt <mg11g16@soton.ac.uk>
2019-02-11 17:37:59 +0000

    Another test

--- api/src/main/java/ml/NeuralNets.java
+++ api/src/main/java/ml/NeuralNets.java
@@ -1,18 +1,12 @@
 package ml;
 
 import org.encog.ConsoleStatusReportable;
-import org.encog.Encog;
 import org.encog.EncogError;
+import org.encog.app.analyst.csv.shuffle.ShuffleCSV;
 import org.encog.engine.network.activation.ActivationSigmoid;
-import org.encog.engine.network.activation.ActivationTANH;
 import org.encog.ml.CalculateScore;
-import org.encog.ml.MLMethod;
-import org.encog.ml.data.MLDataPair;
 import org.encog.ml.data.MLDataSet;
-import org.encog.ml.data.basic.BasicMLDataSet;
 import org.encog.ml.data.specific.CSVNeuralDataSet;
-import org.encog.ml.factory.MLMethodFactory;
-import org.encog.ml.factory.MLTrainFactory;
 import org.encog.ml.svm.SVM;
 import org.encog.ml.svm.training.SVMTrain;
 import org.encog.ml.train.MLTrain;
@@ -23,9 +17,7 @@ import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.TrainingSetScore;
 import org.encog.neural.networks.training.anneal.NeuralSimulatedAnnealing;
-import org.encog.neural.networks.training.propagation.back.Backpropagation;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
-import org.encog.neural.pattern.FeedForwardPattern;
 import org.encog.util.csv.CSVFormat;
 import org.encog.util.normalize.DataNormalization;
 import org.encog.util.normalize.input.InputField;
@@ -33,6 +25,7 @@ import org.encog.util.normalize.input.InputFieldCSV;
 import org.encog.util.normalize.input.InputFieldCSVText;
 import org.encog.util.normalize.output.OutputFieldRangeMapped;
 import org.encog.util.normalize.output.nominal.OutputOneOf;
+import org.encog.util.normalize.segregate.index.IndexSampleSegregator;
 import org.encog.util.normalize.target.NormalizationStorageCSV;
 
 import java.io.File;
@@ -66,7 +59,7 @@ class NeuralNets {
         BasicNetwork network = new BasicNetwork();
         network.addLayer(new BasicLayer(null, true, 24));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 6));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 1));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 2));
         network.getStructure().finalizeStructure();
         network.reset();
 
@@ -95,64 +88,75 @@ class NeuralNets {
         return trainMain.getError();
     }
     
-    static void prepareDataManual() {
+    static void prepareData(boolean test) {
         File source = getSource();
+
+        if (!test) {
+            ShuffleCSV shuffle = new ShuffleCSV();
+            shuffle.setProduceOutputHeaders(false);
+            shuffle.analyze(source, true, CSVFormat.ENGLISH);
+            shuffle.process(new File("shuffled.csv"));
+        }
+
         DataNormalization norm = new DataNormalization();
         
         InputField numAttachments, sizeAttachments, numLinks, numLinksClickHere, numIpUrl, numLinksFileExt, numRecipients,
                 numLinksNonMatching, numConcatenatedUrls, spanTime, numUniqueDomains, numShortenedUrls, maxDots, numCc,
                 subjectLength;
-
+        
         InputFieldCSVText htmlJavascript, htmlBody, fromSenderSimilar, bccExists, fromNameExists, fromNonEnglish, noReply,
                 subjectExists, senderExists, classification;
 
-        norm.addInputField(numAttachments = new InputFieldCSV(true, source, 0));
-        norm.addInputField(sizeAttachments = new InputFieldCSV(true, source, 1));
-        norm.addInputField(numLinks = new InputFieldCSV(true, source, 2));
-        norm.addInputField(numLinksClickHere = new InputFieldCSV(true, source, 3));
-        norm.addInputField(numIpUrl = new InputFieldCSV(true, source, 4));
-        norm.addInputField(numLinksFileExt = new InputFieldCSV(true, source, 5));
-        norm.addInputField(htmlJavascript = new InputFieldCSVText(true, source, 6));
-        norm.addInputField(numRecipients = new InputFieldCSV(true, source, 7));
-        norm.addInputField(htmlBody = new InputFieldCSVText(true, source, 8));
-        norm.addInputField(numLinksNonMatching = new InputFieldCSV(true, source, 9));
-        norm.addInputField(numConcatenatedUrls = new InputFieldCSV(true, source, 10));
-        norm.addInputField(fromSenderSimilar = new InputFieldCSVText(true, source, 11));
-        norm.addInputField(spanTime = new InputFieldCSV(true, source, 12));
-        norm.addInputField(numUniqueDomains = new InputFieldCSV(true, source, 13));
-        norm.addInputField(numShortenedUrls = new InputFieldCSV(true, source, 14));
-        norm.addInputField(bccExists = new InputFieldCSVText(true, source, 15));
-        norm.addInputField(fromNameExists = new InputFieldCSVText(true, source, 16));
-        norm.addInputField(fromNonEnglish = new InputFieldCSVText(true, source, 17));
-        norm.addInputField(maxDots = new InputFieldCSV(true, source, 18));
-        norm.addInputField(numCc = new InputFieldCSV(true, source, 19));
-        norm.addInputField(noReply = new InputFieldCSVText(true, source, 20));
-        norm.addInputField(subjectExists = new InputFieldCSVText(true, source, 21));
-        norm.addInputField(subjectLength = new InputFieldCSV(true, source, 22));
-        norm.addInputField(senderExists = new InputFieldCSVText(true, source, 23));
-        norm.addInputField(classification = new InputFieldCSVText(true, source, 24));
 
-        htmlJavascript.addMapping("TRUE");
-        htmlJavascript.addMapping("FALSE");
-        htmlBody.addMapping("TRUE");
-        htmlBody.addMapping("FALSE");
-        fromSenderSimilar.addMapping("TRUE");
-        fromSenderSimilar.addMapping("FALSE");
-        bccExists.addMapping("TRUE");
-        bccExists.addMapping("FALSE");
-        fromNameExists.addMapping("TRUE");
-        fromNameExists.addMapping("FALSE");
-        fromNonEnglish.addMapping("TRUE");
-        fromNonEnglish.addMapping("FALSE");
-        noReply.addMapping("TRUE");
-        noReply.addMapping("FALSE");
-        subjectExists.addMapping("TRUE");
-        subjectExists.addMapping("FALSE");
-        senderExists.addMapping("TRUE");
-        senderExists.addMapping("FALSE");
-        classification.addMapping("phishing");
-        classification.addMapping("non-phishing");
+        File input = new File("shuffled.csv");
+        
+        norm.addInputField(numAttachments = new InputFieldCSV(true, input, 0));
+        norm.addInputField(sizeAttachments = new InputFieldCSV(true, input, 1));
+        norm.addInputField(numLinks = new InputFieldCSV(true, input, 2));
+        norm.addInputField(numLinksClickHere = new InputFieldCSV(true, input, 3));
+        norm.addInputField(numIpUrl = new InputFieldCSV(true, input, 4));
+        norm.addInputField(numLinksFileExt = new InputFieldCSV(true, input, 5));
+        norm.addInputField(htmlJavascript = new InputFieldCSVText(true, input, 6));
+        norm.addInputField(numRecipients = new InputFieldCSV(true, input, 7));
+        norm.addInputField(htmlBody = new InputFieldCSVText(true, input, 8));
+        norm.addInputField(numLinksNonMatching = new InputFieldCSV(true, input, 9));
+        norm.addInputField(numConcatenatedUrls = new InputFieldCSV(true, input, 10));
+        norm.addInputField(fromSenderSimilar = new InputFieldCSVText(true, input, 11));
+        norm.addInputField(spanTime = new InputFieldCSV(true, input, 12));
+        norm.addInputField(numUniqueDomains = new InputFieldCSV(true, input, 13));
+        norm.addInputField(numShortenedUrls = new InputFieldCSV(true, input, 14));
+        norm.addInputField(bccExists = new InputFieldCSVText(true, input, 15));
+        norm.addInputField(fromNameExists = new InputFieldCSVText(true, input, 16));
+        norm.addInputField(fromNonEnglish = new InputFieldCSVText(true, input, 17));
+        norm.addInputField(maxDots = new InputFieldCSV(true, input, 18));
+        norm.addInputField(numCc = new InputFieldCSV(true, input, 19));
+        norm.addInputField(noReply = new InputFieldCSVText(true, input, 20));
+        norm.addInputField(subjectExists = new InputFieldCSVText(true, input, 21));
+        norm.addInputField(subjectLength = new InputFieldCSV(true, input, 22));
+        norm.addInputField(senderExists = new InputFieldCSVText(true, input, 23));
+        norm.addInputField(classification = new InputFieldCSVText(true, input, 24));
 
+        htmlJavascript.addMapping("1");
+        htmlJavascript.addMapping("0");
+        htmlBody.addMapping("1");
+        htmlBody.addMapping("0");
+        fromSenderSimilar.addMapping("1");
+        fromSenderSimilar.addMapping("0");
+        bccExists.addMapping("1");
+        bccExists.addMapping("0");
+        fromNameExists.addMapping("1");
+        fromNameExists.addMapping("0");
+        fromNonEnglish.addMapping("1");
+        fromNonEnglish.addMapping("0");
+        noReply.addMapping("1");
+        noReply.addMapping("0");
+        subjectExists.addMapping("1");
+        subjectExists.addMapping("0");
+        senderExists.addMapping("1");
+        senderExists.addMapping("0");
+        classification.addMapping("1");
+        classification.addMapping("0");
+        
         norm.addOutputField(new OutputFieldRangeMapped(numAttachments, 0, 1));
         norm.addOutputField(new OutputFieldRangeMapped(sizeAttachments, 0, 1));
         norm.addOutputField(new OutputFieldRangeMapped(numLinks, 0, 1));
@@ -177,21 +181,29 @@ class NeuralNets {
         norm.addOutputField(new OutputOneOf(subjectExists, 1, 0));
         norm.addOutputField(new OutputFieldRangeMapped(subjectLength, 0, 1));
         norm.addOutputField(new OutputOneOf(senderExists, 1, 0));
-        norm.addOutputField(new OutputOneOf(classification, 1, 0));
+        norm.addOutputField(new OutputOneOf(classification, 1, 0), true);
 
-        File output = new File("normalised_all.csv");
-        norm.setCSVFormat(CSVFormat.DECIMAL_POINT);
+        File output;
+        if (test) {
+            output = new File("normalised_test.csv");
+            norm.addSegregator(new IndexSampleSegregator(7, 10, 10));
+        } else {
+            output = new File("normalised_train.csv");
+            norm.addSegregator(new IndexSampleSegregator(0, 7, 10));
+        }
+
         norm.setTarget(new NormalizationStorageCSV(CSVFormat.ENGLISH, output));
+        norm.setCSVFormat(CSVFormat.ENGLISH);
         norm.setReport(new ConsoleStatusReportable());
         norm.process();
     }
 
     static CSVNeuralDataSet prepareNeuralTrainData() {
-        return new CSVNeuralDataSet("normalised_train.csv", 24, 1, false);
+        return new CSVNeuralDataSet("normalised_train.csv", 24, 2, false);
     }
 
     static CSVNeuralDataSet prepareNeuralTestData() {
-        return new CSVNeuralDataSet("normalised_test.csv", 24, 1, false);
+        return new CSVNeuralDataSet("normalised_test.csv", 24, 2, false);
     }
 
     private static File getSource() {
