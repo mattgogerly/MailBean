83289caf2f0771f2f6ef05dc45fd62c9c3c10c2e
Matt <mg11g16@soton.ac.uk>
2019-02-22 12:20:14 +0000

    Begin implementation of authentication via other providers.

--- frontend/src/app/auth/other-auth/other-auth.component.ts
+++ frontend/src/app/auth/other-auth/other-auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountHandler } from '../utils/AccountHandler';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-other-auth',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherAuthComponent implements OnInit {
 
-  constructor() { }
+  public authStatus = 'pending';
+  public step = 1;
+  public name: string;
+  public email: string;
+  public password: string;
+  private AccountHandler: AccountHandler;
+
+  constructor(private router: Router) {
+    this.AccountHandler = new AccountHandler();
+  }
 
   ngOnInit() {
+  }
+
+  async submit() {
+    const account = await this.AccountHandler.constructAccountFromInfo({
+      name: this.name,
+      email: this.email,
+      password: this.password
+    });
+
+    if (account.settings.imap_host) {
+      this.router.navigate(['/auth/success']);
+    } else {
+      this.step = 2;
+    }
   }
 
 }
