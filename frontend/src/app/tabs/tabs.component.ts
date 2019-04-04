7dfa841fd35b3a3cece0330cbe608a37cea447f0
Matt <mg11g16@soton.ac.uk>
2019-04-04 15:11:32 +0100

    Add close button. Add tab to add a new account. Rename css to scss and minor readability improvements.

--- frontend/src/app/tabs/tabs.component.ts
+++ frontend/src/app/tabs/tabs.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { unionBy } from 'lodash';
+import { DeleteAccountPending, GetAccountsPending, SetCurrentAccountPending } from '../redux/actions/account.actions';
+import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
-import { GetAccountsPending } from '../redux/actions/account.actions';
+import { MatTabGroup } from '@angular/material';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements OnInit {
 
-  tabs = [];
+  tabs = [
+    {
+      name: 'Add Account',
+      provider: '+',
+      id: ''
+    }
+  ];
   selected = new FormControl(0);
 
-  constructor(private store: Store<any>) {
+  constructor(private store: Store<any>, private router: Router) {
     this.store.select('accountsInfo')
       .subscribe(accountsInfo => {
         this.tabs = unionBy(accountsInfo.accounts, this.tabs, 'id');
@@ -23,6 +31,28 @@ export class TabsComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(new GetAccountsPending());
+  }
+
+  changeTab(event) {
+    if (event === this.tabs.length - 1) {
+      this.addAccount();
+    } else {
+      this.store.dispatch(new SetCurrentAccountPending(this.tabs[event].id));
+      this.selected.setValue(event);
+    }
+  }
+
+  closeTab(index) {
+    this.store.dispatch(new DeleteAccountPending(this.tabs[index].id));
+    this.tabs.splice(index, 1);
+
+    if (this.tabs.length === 1) {
+      this.router.navigate(['/auth']);
+    }
+  }
+
+  addAccount() {
+    this.router.navigate(['/auth'], {queryParams: {canCancel: true}});
   }
 
 }
