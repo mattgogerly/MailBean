01b772860c7064f7ede02d6433f7c8c2a3e68e9d
Matt <mg11g16@soton.ac.uk>
2019-04-04 15:08:37 +0100

    Hook actions up to the API. Rename css to scss. Format email addresses.

--- frontend/src/app/message/message.component.ts
+++ frontend/src/app/message/message.component.ts
@@ -1,33 +1,100 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { DetailedMessage } from '../redux/models/detailed-message';
+import { DeleteMessagePending, ToggleComposing } from '../redux/actions/message.actions';
+import { WebAddressToEmailPipe } from '../utils/web-address-to-email.pipe';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
 
-  @ViewChild('message') dataContainer: ElementRef;
-  message: DetailedMessage = new DetailedMessage();
+  message: DetailedMessage;
+  from: string;
+  to: string[] = [];
+  cc: string[] = [];
+  subject = '';
+  content = '';
+  display = false;
+  originatedHere = false;
 
-  constructor(private store: Store<any>) { }
+  composing = false;
+  reply = false;
+  replyAll = false;
+  replyTo: string[] = [];
+  replyCc: string[] = [];
+  replySubject = '';
+  replyContent = '';
+
+  constructor(private store: Store<any>, private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.store.pipe(select((state: any) => state.messages.currentMessage))
-      .subscribe(message => {
-        this.message = message;
-        this.loadHtml();
+    this.store.pipe(select((state: any) => state.messages))
+      .subscribe(messageState => {
+          this.message = messageState.messages.find(m => m.uid === messageState.currentMessage);
+
+          if (this.message !== undefined) {
+            this.from = WebAddressToEmailPipe.prototype.transform(this.message.sender);
+            this.to = this.message.to.map(m => WebAddressToEmailPipe.prototype.transform(m));
+            this.cc = this.message.cc.map(m => WebAddressToEmailPipe.prototype.transform(m));
+            this.display = true;
+          } else {
+            this.display = false;
+          }
+      });
+
+    this.store.pipe(select((state: any) => state.messages.composing))
+      .subscribe(toggle => {
+        if (!this.originatedHere) {
+          this.replyTo = [];
+          this.replyCc = [];
+          this.replySubject = '';
+          this.replyContent = '';
+          this.composing = toggle;
+
+          this.changeDetector.detectChanges();
+        }
+
+        this.originatedHere = false;
       });
   }
 
-  loadHtml() {
-    if (this.message.content === undefined) {
-      this.message.content = '';
-    }
+  openComposer(type: string) {
+    this.store.dispatch(new ToggleComposing(true));
 
-    this.dataContainer.nativeElement.innerHTML = this.message.content;
+    switch (type) {
+      case 'reply':
+        this.reply = true;
+        this.replyAll = false;
+        this.replyTo = [this.message.sender];
+        this.replyCc = this.message.cc;
+        this.replySubject = 'Re: ' + this.message.subject;
+        this.replyContent = this.message.content;
+        break;
+      case 'forward':
+        this.reply = false;
+        this.replyAll = false;
+        this.replyTo = [];
+        this.replyCc = this.message.cc;
+        this.replySubject = 'Fwd: ' + this.message.subject;
+        this.replyContent = this.message.content;
+        break;
+      default:
+        this.reply = false;
+        this.replyAll = false;
+        this.replyTo = [];
+        this.replyCc = [];
+        this.replySubject = '';
+        this.replyContent = '';
+        break;
+    }
+  }
+
+  deleteMessage() {
+    this.store.dispatch(new DeleteMessagePending(this.message));
+    this.message = null;
   }
 
 }
