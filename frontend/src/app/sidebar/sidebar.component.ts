5bf27c91c99b38d4827a44505007b5b850349741
Matt <mg11g16@soton.ac.uk>
2019-04-04 15:11:05 +0100

    Minor readability improvements. Rename css to scss.

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Folder } from '../redux/models/folder';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
 
