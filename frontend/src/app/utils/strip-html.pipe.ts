6c3dcb5ca5f77463b2cec36a0baa7cbeefdc7048
Matt Gogerly <matthewgogerly@gmail.com>
2019-04-09 18:03:34 +0100

    Add extra regex to remove <style> tags (css), excess whitespace and newlines.

--- frontend/src/app/utils/strip-html.pipe.ts
+++ frontend/src/app/utils/strip-html.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class StripHtmlPipe implements PipeTransform {
   transform(value: string): any {
+    // remove <style> tags and their contents
+    value = value.replace(/<style[\s\S]*<\/style>/g, '').replace(/\r?\n|\r/g, ' ').replace(/\s\s+/g, ' ');
+
     const temp = document.createElement('div');
     temp.innerHTML = value;
     return temp.textContent || temp.innerText || '';
