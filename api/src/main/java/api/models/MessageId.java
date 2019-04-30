3a41b34cb92fab7e95262d80b74f0852b252e799
Matt Gogerly <matthewgogerly@gmail.com>
2019-04-30 12:29:00 +0100

    Fix issue with composite key and add Javadoc

--- api/src/main/java/api/models/MessageId.java
+++ api/src/main/java/api/models/MessageId.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+ * Composite ID for a message as there is no guarantee that two email providers won't have conflicting UIDs.
+ *
+ * @author mattgogerly
+ */
 @Getter
 @Setter
 @Embeddable
@@ -18,16 +20,24 @@ public class MessageId implements Serializable {
     @Column(name = "uid")
     private Long uid;
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "randomSeed")
     @JsonIgnore
-    private Integer randomSeed;
+    private Double randomSeed;
 
+    /**
+     * Intentionally empty constructor
+     */
     public MessageId() {
 
     }
 
+    /**
+     * Create a new MessageId
+     *
+     * @param uid uid of the message
+     */
     public MessageId(Long uid) {
         this.uid = uid;
+        this.randomSeed = Math.random();
     }
 }
